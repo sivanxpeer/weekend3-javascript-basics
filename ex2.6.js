c11a43b7a42a2359cf75a001926b8b31c13c5a28
sivanxpeer <sivan.x.peer@gmail.com>
2021-11-12 10:46:53 +0200

    ex2.6

---
+++ ex2.6.js
@@ -0,0 +1,25 @@
+// Ex2.6 - Years and Centuries 
+
+// The first century spans from the year 1 up to and including the year 100, The second - from the 
+// year 101 up to and including the year 200, etc. 
+// Task : 
+// Given a year, return the century it is in. 
+// Input , Output Examples :: 
+// centuryFromYear(1705)  returns (18) 
+// centuryFromYear(  1900)  returns (19) 
+// centuryFromYear(1601)  returns (17) 
+// centuryFromYear(2000)  returns (20) 
+
+function findCentury(year) {
+    if (year % 100 == 0)
+        return year / 100;
+    else return Math.floor((year / 100)+1);
+}
+
+
+//tests:
+console.log(findCentury(1705));
+console.log(findCentury(1900));
+console.log(findCentury(1601));
+console.log(findCentury(2000));
+console.log(findCentury(2001));
