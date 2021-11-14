a47221af4e8b039c7b6c04bece59dd6dd79d2dc1
sivanxpeer <sivan.x.peer@gmail.com>
2021-11-14 08:58:13 +0200

    ex5.3

--- ex5.3.js
+++ ex5.3.js
@@ -2,25 +2,38 @@
 // Complete the method/function so that it converts dash/underscore delimited words into camel 
 // casing. The first word within the output should be capitalized only if the original word was 
 // capitalized (known as Upper Camel Case, also often referred to as Pascal case). 
- 
+
 // Examples 
 // toCamelCase("the-stealth-warrior") // returns "theStealthWarrior" 
- 
+
 // toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior" 
 
-function toCamelCase(stringToCamel){
 
-    let array = stringToCamel.split("\-\_");
-    console.log(array);
-    for(let i=0;i<stringToCamel.length;i++){
-        if(stringToCamel[i]=="_"||stringToCamel[i]=="-"){
-            retString[i]= stringToCamel[i+1].toUpperCase();
-            continue;
-        }
-        retString=stringToCamel[i];
-    }
-    return retString.toString();
+camelize = function camelize(str) {
+    return str.replace(/\W+(.)/g, function (match, chr) {
+        return chr.toUpperCase();
+    });
 }
 
-console.log(toCamelCase("The_Stealth_Warrior"));
-console.log(toCamelCase("the-stealth-warrior"));
+
+// console.log(camelize("Concurrent hash maps")) 
+console.log(camelize("The_Stealth_Warrior"));
+console.log(camelize("the-stealth-warrior"));
+
+// function toCamelCase(stringToCamel){
+
+//     let array = stringToCamel.split(" ");
+//     let retString="";
+//     console.log(array);
+//     for(let i=0;i<stringToCamel.length;i++){
+//         if(stringToCamel[i]=="_"||stringToCamel[i]=="-"){
+//             retString[i] += stringToCamel[i+1].toUpperCase();
+//             continue;
+//         }
+//        else retString+=stringToCamel[i];
+//     }
+//     return retString.toString();
+// }
+
+// console.log(toCamelCase("The_Stealth_Warrior"));
+// console.log(toCamelCase("the-stealth-warrior"));
